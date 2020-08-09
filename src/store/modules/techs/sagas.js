f1871ab4f62dc46845b0ea0d62453cdf4d6e6cb9
Renato Castro <renatocastro02@hotmail.com>
2020-08-09 13:57:52 -0300

    coverage

---
+++ src/store/modules/techs/sagas.js
@@ -0,0 +1,13 @@
+import { call, put } from 'redux-saga/effects';
+import api from '~/services/api';
+import  { getTechSuccess, getTechsFailure } from '~/store/modules/techs/actions';
+
+export function* getTechs() {
+    try{
+        const response = yield call(api.get, 'techs');
+
+        yield put(getTechSuccess(response.data));
+    } catch(err) {
+        yield put(getTechsFailure());
+    }
+}
